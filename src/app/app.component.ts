7dbf80c553afbe02f1c218d28e6a4537a31c88ed
hegigj <hgjoka16@gmail.com>
2023-06-01 19:20:37 +0200

    finish app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { UserModel } from './user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-academy-03';
+export class AppComponent implements OnInit {
+  users: UserModel[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(users => this.users = users);
+  }
 }
