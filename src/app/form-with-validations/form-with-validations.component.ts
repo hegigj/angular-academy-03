c646f7daeeb3e9f717a8dd37d9fba5c3fb69e292
hegigj <hgjoka16@gmail.com>
2023-06-06 18:53:26 +0200

    validation

--- src/app/form-with-validations/form-with-validations.component.ts
+++ src/app/form-with-validations/form-with-validations.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,13 +6,20 @@ import { NgForm } from '@angular/forms';
   templateUrl: './form-with-validations.component.html',
   styleUrls: ['./form-with-validations.component.scss']
 })
-export class FormWithValidationsComponent implements OnInit {
-  @ViewChild('userForm')
+export class FormWithValidationsComponent implements OnInit, AfterViewInit {
+  @ViewChild('userFormRef')
   userForm!: NgForm;
 
-  constructor() { }
+  constructor() {
+    console.log(this.userForm);
+  }
 
   ngOnInit(): void {
+    console.log(this.userForm);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('Form with validation: ', this.userForm);
   }
 
   add(): void {}
