111550e96dc16448d72badfdbf37f01a3aa1fc66
hegigj <hgjoka16@gmail.com>
2023-06-09 18:59:39 +0200

    form group & array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-academy-03';
+  cvForm: FormGroup;
+
+  constructor() {
+    this.cvForm = new FormGroup({
+      personalData: new FormGroup({
+        firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        // birthDate: new FormControl('', [Validators.required, Validators.max('2005-01-01')])
+        birthDate: new FormControl('', [Validators.required])  
+      }),
+      education: new FormArray([
+        new FormGroup({
+          university: new FormControl('', Validators.required),
+          degree: new FormControl('', Validators.required),
+          start: new FormControl('', Validators.required),
+          end: new FormControl({ value: '', disabled: true }),
+          ongoing: new FormControl({ value: '', disabled: true }),
+        })
+      ])
+    });
+  }
+
+  getFormArray(control: AbstractControl | null): FormArray {
+    return control as FormArray;
+  }
+
+  create(): void {
+    console.log(this.cvForm);
+  }
 }
---
+++ src/app/cv.model.ts
@@ -0,0 +1,5 @@
+import { FormControl } from "@angular/forms";
+
+// export interface CvFormModel {
+//     firstName: FormControl<string>;
+// }
